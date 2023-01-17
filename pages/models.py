7d731f976218401cd933977d3f033e73528b7994
ricardogrados <ricardo.grados.g@uni.pe>
2023-01-17 13:59:43 -0500

    add set_null in fk

--- pages/models.py
+++ pages/models.py
@@ -8,10 +8,10 @@ class BannerSeccion(models.Model):
     subtitulo = models.CharField(max_length=200, verbose_name='subtítulo', null=True, blank=True)
     descripcion = models.TextField(null=True, verbose_name='descripción', blank=True)
     primer_boton = models.CharField(max_length=255, verbose_name='primer botón', null=True, blank=True)
-    primer_boton_url = models.ForeignKey(URL, on_delete=models.CASCADE, verbose_name='url del primer botón', related_name='url_primer_boton_banner', null=True, blank=True)
+    primer_boton_url = models.ForeignKey(URL, on_delete=models.SET_NULL, verbose_name='url del primer botón', related_name='url_primer_boton_banner', null=True, blank=True)
     primer_boton_icono = FAIconField(verbose_name='ícono primer botón', null=True, blank=True)
     segundo_boton = models.CharField(max_length=255, verbose_name='segundo botón', null=True, blank=True)
-    segundo_boton_url = models.ForeignKey(URL, on_delete=models.CASCADE, verbose_name='url del segundo botón', related_name='url_segundo_boton_banner', null=True, blank=True)
+    segundo_boton_url = models.ForeignKey(URL, on_delete=models.SET_NULL, verbose_name='url del segundo botón', related_name='url_segundo_boton_banner', null=True, blank=True)
     segundo_boton_icono = FAIconField(verbose_name='ícono segundo botón', null=True, blank=True)
     titulo_imagen = models.CharField(max_length=255, verbose_name='título imagen', null=True, blank=True)
     alt_imagen = models.CharField(max_length=255, verbose_name='alt imagen', null=True, blank=True)
@@ -24,21 +24,21 @@ class BannerSeccion(models.Model):
         return str(self.titulo)
     
 class Beneficio(models.Model):
-    titulo = models.CharField(max_length=200)
-    descripcion = models.TextField()
+    titulo = models.CharField(max_length=200, null=True, blank=True)
+    descripcion = models.TextField(null=True, blank=True)
     titulo_imagen = models.CharField(max_length=255, verbose_name='título imagen', null=True, blank=True)
     alt_imagen = models.CharField(max_length=255, verbose_name='alt imagen', null=True, blank=True)
     imagen = models.ImageField(upload_to='beneficio_seccion', verbose_name='imagen', null=True, blank=True)
     boton = models.CharField(max_length=255, verbose_name='primer botón', null=True, blank=True)
-    boton_url = models.ForeignKey(URL, on_delete=models.CASCADE, verbose_name='url del botón', related_name='url_boton_beneficio', null=True, blank=True)
+    boton_url = models.ForeignKey(URL, on_delete=models.SET_NULL, verbose_name='url del botón', related_name='url_boton_beneficio', null=True, blank=True)
     boton_icono = FAIconField(verbose_name='ícono botón', null=True, blank=True)
 
     def __str__(self):
         return str(self.titulo)
     
 class BeneficioSeccion(models.Model):
-    titulo = models.CharField(max_length=200)
-    descripcion = models.TextField()
+    titulo = models.CharField(max_length=200, null=True, blank=True)
+    descripcion = models.TextField(null=True, blank=True)
     beneficios = models.ManyToManyField(Beneficio, verbose_name='Beneficios', blank=True)
 
     def __str__(self):
@@ -50,10 +50,10 @@ class BasicoSeccion(models.Model):
     subtitulo = models.CharField(max_length=200, verbose_name='subtítulo', null=True, blank=True)
     descripcion = models.TextField(null=True, verbose_name='descripción', blank=True)
     primer_boton = models.CharField(max_length=255, verbose_name='primer botón', null=True, blank=True)
-    primer_boton_url = models.ForeignKey(URL, on_delete=models.CASCADE, verbose_name='url del primer botón', related_name='url_primer_boton_basico', null=True, blank=True)
+    primer_boton_url = models.ForeignKey(URL, on_delete=models.SET_NULL, verbose_name='url del primer botón', related_name='url_primer_boton_basico', null=True, blank=True)
     primer_boton_icono = FAIconField(verbose_name='ícono primer botón', null=True, blank=True)
     segundo_boton = models.CharField(max_length=255, verbose_name='segundo botón', null=True, blank=True)
-    segundo_boton_url = models.ForeignKey(URL, on_delete=models.CASCADE, verbose_name='url del segundo botón', related_name='url_segundo_boton_basico', null=True, blank=True)
+    segundo_boton_url = models.ForeignKey(URL, on_delete=models.SET_NULL, verbose_name='url del segundo botón', related_name='url_segundo_boton_basico', null=True, blank=True)
     segundo_boton_icono = FAIconField(verbose_name='ícono segundo botón', null=True, blank=True)
     titulo_imagen = models.CharField(max_length=255, verbose_name='título imagen', null=True, blank=True)
     alt_imagen = models.CharField(max_length=255, verbose_name='alt imagen', null=True, blank=True)
@@ -70,7 +70,7 @@ class Servicio(models.Model):
     titulo = models.CharField(max_length=200, verbose_name='titulo', null=True, blank=True)
     descripcion = models.TextField(null=True, verbose_name='descripción', blank=True)
     boton = models.CharField(max_length=255, verbose_name='botón', null=True, blank=True)
-    boton_url = models.ForeignKey(URL, on_delete=models.CASCADE, verbose_name='url del primer botón', related_name='url_boton_servicios_pages', null=True, blank=True)
+    boton_url = models.ForeignKey(URL, on_delete=models.SET_NULL, verbose_name='url del primer botón', related_name='url_boton_servicios_pages', null=True, blank=True)
     boton_icono = FAIconField(verbose_name='ícono botón', null=True, blank=True)
     titulo_imagen = models.CharField(max_length=255, verbose_name='título imagen', null=True, blank=True)
     alt_imagen = models.CharField(max_length=255, verbose_name='alt imagen', null=True, blank=True)
@@ -84,10 +84,10 @@ class ServiciosSeccion(models.Model):
     destacado = models.CharField(max_length=200, verbose_name='destacado', null=True, blank=True)
     descripcion = models.TextField(null=True, verbose_name='descripción', blank=True)
     primer_boton = models.CharField(max_length=255, verbose_name='primer botón', null=True, blank=True)
-    primer_boton_url = models.ForeignKey(URL, on_delete=models.CASCADE, verbose_name='url del primer botón', related_name='url_primer_boton_servicios_pages', null=True, blank=True)
+    primer_boton_url = models.ForeignKey(URL, on_delete=models.SET_NULL, verbose_name='url del primer botón', related_name='url_primer_boton_servicios_pages', null=True, blank=True)
     primer_boton_icono = FAIconField(verbose_name='ícono primer botón', null=True, blank=True)
     segundo_boton = models.CharField(max_length=255, verbose_name='segundo botón', null=True, blank=True)
-    segundo_boton_url = models.ForeignKey(URL, on_delete=models.CASCADE, verbose_name='url del segundo botón', related_name='url_segundo_boton_servicios_pages', null=True, blank=True)
+    segundo_boton_url = models.ForeignKey(URL, on_delete=models.SET_NULL, verbose_name='url del segundo botón', related_name='url_segundo_boton_servicios_pages', null=True, blank=True)
     segundo_boton_icono = FAIconField(verbose_name='ícono segundo botón', null=True, blank=True)
     servicios = models.ManyToManyField(Servicio, verbose_name='Servicios', blank=True)
     
@@ -100,7 +100,7 @@ class Bloques(models.Model):
     titulo = models.CharField(max_length=200, verbose_name='titulo', null=True, blank=True)
     descripcion = models.TextField(null=True, verbose_name='descripción', blank=True)
     boton = models.CharField(max_length=255, verbose_name='botón', null=True, blank=True)
-    boton_url = models.ForeignKey(URL, on_delete=models.CASCADE, verbose_name='url del botón', related_name='url_boton_bloque', null=True, blank=True)
+    boton_url = models.ForeignKey(URL, on_delete=models.SET_NULL, verbose_name='url del botón', related_name='url_boton_bloque', null=True, blank=True)
     boton_icono = FAIconField(verbose_name='ícono botón', null=True, blank=True)
     titulo_imagen = models.CharField(max_length=255, verbose_name='título imagen', null=True, blank=True)
     alt_imagen = models.CharField(max_length=255, verbose_name='alt imagen', null=True, blank=True)
@@ -140,8 +140,8 @@ class OpcionGrupo(models.Model):
         return str(self.nombre)
 
 class Opcion(models.Model):
-    grupo = models.ForeignKey(OpcionGrupo, on_delete=models.CASCADE, null=True, blank=True)
-    nombre = models.CharField(max_length=200, verbose_name='nombre')
+    grupo = models.ForeignKey(OpcionGrupo, on_delete=models.SET_NULL, null=True, blank=True)
+    nombre = models.CharField(max_length=200, verbose_name='nombre', null=True, blank=True)
     
     def __str__(self):
         return f'{self.grupo} - {self.nombre}'
@@ -157,7 +157,7 @@ class Campo(models.Model):
         FILE = 7
     
     formulario = models.ForeignKey(Formulario, on_delete=models.CASCADE)
-    tipo = models.IntegerField(choices=TypeCampo.choices)
+    tipo = models.IntegerField(choices=TypeCampo.choices, null=True, blank=True)
     label = models.CharField(max_length=200, verbose_name='label', null=True, blank=True)
     opciones = models.ManyToManyField(Opcion, blank=True)
     
@@ -165,7 +165,7 @@ class Campo(models.Model):
         return str(self.label)
     
 class Resultado(models.Model):
-    formulario = models.ForeignKey(Formulario, on_delete=models.CASCADE)
+    formulario = models.ForeignKey(Formulario, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateField(verbose_name='fecha de creación', null=True, blank=True)
     
     def __str__(self):
@@ -175,15 +175,15 @@ class Resultado(models.Model):
         return ', '.join([f'{detail.campo.label}: {detail.valor}' for detail in self.details.all()])
 
 class ResultadoDetalle(models.Model):
-    resultado = models.ForeignKey(Resultado,related_name='details', on_delete=models.CASCADE)
-    campo = models.ForeignKey(Campo,verbose_name='campo', on_delete=models.CASCADE)
+    resultado = models.ForeignKey(Resultado,related_name='details', on_delete=models.CASCADE, null=True, blank=True)
+    campo = models.ForeignKey(Campo,verbose_name='campo', on_delete=models.CASCADE, null=True, blank=True)
     valor = models.CharField(max_length=200, verbose_name='valor', null=True, blank=True)
     
     def __str__(self):
         return str(self.valor)
     
 class FormularioSeccion(models.Model):
-    formulario = models.ForeignKey(Formulario,related_name='formulario', on_delete=models.CASCADE)
+    formulario = models.ForeignKey(Formulario,related_name='formulario', on_delete=models.CASCADE, null=True, blank=True)
     titulo = models.CharField(max_length=200, verbose_name='título', null=True, blank=True)
     descripcion = models.TextField(null=True, verbose_name='descripción', blank=True)
     
@@ -191,8 +191,8 @@ class FormularioSeccion(models.Model):
         return str(self.titulo)
     
 class Page(models.Model):
-    titulo = models.CharField(max_length=200)
-    descripcion = models.TextField()
+    titulo = models.CharField(max_length=200, null=True, blank=True)
+    descripcion = models.TextField(null=True, blank=True)
     slug = models.SlugField(max_length=255, null=True, blank=True, unique=True) 
     header = models.BooleanField(default=False)
     footer= models.BooleanField(default=False)
@@ -210,7 +210,7 @@ class EtiquetasMeta(models.Model):
 
     pagina = models.ForeignKey(Page, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, verbose_name='name', null=True, blank=True)
-    content = models.CharField(max_length=200, verbose_name='content')
+    content = models.CharField(max_length=200, verbose_name='content',null=True, blank=True)
     property = models.CharField(max_length=200, verbose_name='property', null=True, blank=True)
     itemprop = models.CharField(max_length=200, verbose_name='itemprop', null=True, blank=True)
 
@@ -229,12 +229,12 @@ class PageDetail(models.Model):
 
     pagina = models.ForeignKey(Page, on_delete=models.CASCADE)
     type = models.IntegerField(choices=Type.choices)
-    banner_seccion = models.ForeignKey(BannerSeccion, on_delete=models.CASCADE, null=True, blank=True)
-    beneficio_seccion = models.ForeignKey(BeneficioSeccion, on_delete=models.CASCADE, null=True, blank=True)
-    basico_seccion = models.ForeignKey(BasicoSeccion, on_delete=models.CASCADE, null=True, blank=True)
-    servicios_seccion = models.ForeignKey(ServiciosSeccion, on_delete=models.CASCADE, null=True, blank=True)
-    bloques_seccion = models.ForeignKey(BloquesSeccion, on_delete=models.CASCADE, null=True, blank=True)
-    formulario_seccion = models.ForeignKey(FormularioSeccion, on_delete=models.CASCADE, null=True, blank=True)
+    banner_seccion = models.ForeignKey(BannerSeccion, on_delete=models.SET_NULL, null=True, blank=True)
+    beneficio_seccion = models.ForeignKey(BeneficioSeccion, on_delete=models.SET_NULL, null=True, blank=True)
+    basico_seccion = models.ForeignKey(BasicoSeccion, on_delete=models.SET_NULL, null=True, blank=True)
+    servicios_seccion = models.ForeignKey(ServiciosSeccion, on_delete=models.SET_NULL, null=True, blank=True)
+    bloques_seccion = models.ForeignKey(BloquesSeccion, on_delete=models.SET_NULL, null=True, blank=True)
+    formulario_seccion = models.ForeignKey(FormularioSeccion, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return str(self.pagina.titulo)
